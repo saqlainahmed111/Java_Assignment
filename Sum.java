eb117b969e4d138086579a0d768f5493b7420b33
Saqlain Ahmed <60665443+saqlainahmed111@users.noreply.github.com>
2021-09-28 23:34:01 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int sum=0,i;
+        Scanner sc=new Scanner(System.in);
+        int n= sc.nextInt();
+        
+        for(i=2;i<=n;i++)
+        {
+            sum=sum+i;
+            i++;
+        }
+                    System.out.println(sum);
+    }
+}
